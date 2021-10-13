669932f229f6d5d83a87a7cf9264850f6fab0946
Roman Mazeev <romanmazeev@icloud.com>
2021-10-13 10:28:21 +0200

    First commit

---
+++ ADAStudents/ADAStudentsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ADAStudentsApp.swift
+//  ADAStudents
+//
+//  Created by Roman Mazeev on 13/10/21.
+//
+
+import SwiftUI
+
+@main
+struct ADAStudentsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
